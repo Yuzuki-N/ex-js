1c6dee5948bc73de9278fd1e4c00e4c16b8594a5
yuzuki <nanami2962@gmail.com>
2020-12-30 11:15:29 +0800

    js is so hard to understand

---
+++ ch3/test.js
@@ -0,0 +1,19 @@
+// if (true) {
+//     let a = 10
+//     console.log(a)
+// }
+
+// console.log(a)
+
+// if (true) {
+//     console.log(a);
+//     let a = 10;
+// }
+
+// for (const key of [1, 2, 3, 4, 5]) {
+//     console.log(key)
+// }
+
+for (const key in {a:1, b:2}) {
+    console.log(key)
+}
